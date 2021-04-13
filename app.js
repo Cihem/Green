9f54ee82ab2af6e46161bac2b5361fdd4b0527ae
Cihem <cihemzine@gmail.com>
2021-04-13 11:33:49 +0100

    saved

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
 
 
 app.use(logger('dev'));
